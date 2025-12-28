6b89fd4d9177b8b936961666c8f2b6c3ddcea8a6
Ronak-08 <ronakameta65@gmail.com>
2025-12-28 15:31:55 +0530

    bug fixes

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,54 +1,94 @@
+// Disables access to DOM typings like `HTMLElement` which are not available
+// inside a service worker and instantiates the correct globals
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+// Ensures that the `$service-worker` import has proper type definitions
+/// <reference types="@sveltejs/kit" />
+
+// Only necessary if you have an import from `$env/static/public`
+/// <reference types="../.svelte-kit/ambient.d.ts" />
+
 import { build, files, version } from '$service-worker';
 
+// This gives `self` the correct types
+const self = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (globalThis.self));
+
+// Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
-const ASSETS = [...build, ...files];
+
+const ASSETS = [
+  ...build, // the app itself
+  ...files  // everything in `static`
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  // Create a new cache and add all files to it
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
+
+  event.waitUntil(addFilesToCache());
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE) return caches.delete(key);
-        })
-      );
-    })
-  );
+  // Remove previous cached data from disk
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
+    }
+  }
+
+  event.waitUntil(deleteOldCaches());
 });
 
 self.addEventListener('fetch', (event) => {
+  // ignore POST requests etc
   if (event.request.method !== 'GET') return;
 
-  event.respondWith(
-    (async () => {
-      const url = new URL(event.request.url);
-      const cache = await caches.open(CACHE);
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
 
-      if (ASSETS.includes(url.pathname)) {
-        const cached = await cache.match(url.pathname);
-        if (cached) return cached;
-      }
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      const response = await cache.match(url.pathname);
 
-      try {
-        const response = await fetch(event.request);
-        if (response.status === 200 && response.type === 'basic') {
-          try {
-            await cache.put(event.request, response.clone());
-          } catch (err) {
-            console.warn('Caching failed:', err);
-          }
-        }
+      if (response) {
         return response;
-      } catch (e) {
-        const cached = await cache.match(event.request);
-        if (cached) return cached;
-        throw e;
       }
-    })()
-  );
+    }
+
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request);
+
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
+      if (!(response instanceof Response)) {
+        throw new Error('invalid response from fetch');
+      }
+
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
+
+      return response;
+    } catch (err) {
+      const response = await cache.match(event.request);
+
+      if (response) {
+        return response;
+      }
+
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
+      throw err;
+    }
+  }
+
+  event.respondWith(respond());
 });
